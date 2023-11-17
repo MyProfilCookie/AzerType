0db674b60ab02292c752dbb6b52d8188d133f82b
MyProfilCookie <parigopsg24@gmail.com>
2023-11-17 13:18:43 +0100

    mise à jour du nouveau css

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu()
+
+let zoneInput = document.getElementById("inputEcriture")
+console.log(zoneInput)
+
+let zoneBtnValider= document.getElementById("btnValiderMot")
+console.log(zoneBtnValider)
+
